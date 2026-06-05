7e9db522b3ef30d1423dd661140518b81f0c211d
Federico López Rubio <fedelopezrubio@gmail.com>
2026-06-05 16:05:07 -0300

    Validacion de datos
    Condicionales para validar datos antes de ingresarlos a la matriz. En caso de fallar, se vuelve a pedir la totalidad de los datos. 
    Tambien se crea la lista de metodos validos para ingresar por teclado el numero correspondiente a cada uno

--- carga_matriz.py
+++ carga_matriz.py
@@ -7,11 +7,15 @@ def carga_matriz():
     nombre = input('Ingrese el nombre oficial del activo o fin para finalizar: ').upper()
 
     while nombre.lower() != 'fin':
+
+        datos_validos = True
+
         ticker = input('Ingrese el ticker del activo: ').upper()
-        valor_ref = int(input('Ingres el valor de referencia base (Ej: USD) del activo: '))
-        vol_act = int(input('Ingrese el volumen de actividad de las ultimas 24hs: '))
-        met_op = input('Ingrese la metodologia de operacion asignada (Ej: Scalping): ')
-        unidades = int(input('Ingrese las unidades totales en tesoreria: '))
+        valor_ref = float(input('Ingres el valor de referencia base (Ej: USD) del activo: '))
+        vol_act = float(input('Ingrese el volumen de actividad de las ultimas 24hs: '))
+        metodos_validos = ['1: Scalping', '2: Day Trading', '3: Swing Trading', '4: HODL']
+        met_op = int(input(f'Ingrese la metodologia de operacion asignada {metodos_validos}: '))
+        unidades = float(input('Ingrese las unidades totales en tesoreria: '))
         punt_conf = int(input('Ingrese el puntaje de confianza del 1 al 10: '))
 
         #La variable activo podria eliminarse y directamente agregar los datos a la tabla, pero queda ordenado 
@@ -20,13 +24,45 @@ def carga_matriz():
             ticker,
             valor_ref,
             vol_act,
-            met_op,
+            metodos_validos[met_op - 1],
             unidades,
             punt_conf
        ]
-    
-        tabla_activos.append(activo)
-        print('Activo cargado correctamente')
+
+        if nombre == '':
+            datos_validos = False
+            print('El nombre del activo no puede estar vacío. Intente nuevamente.')
+        if int(nombre):
+            datos_validos = False
+            print('El nombre del activo no puede ser un número. Intente nuevamente.')
+        if len(ticker) != 3:
+            datos_validos = False
+            print('El ticker debe tener exactamente 3 caracteres. Intente nuevamente.')
+        if int(ticker):
+            datos_validos = False
+            print('El ticker no puede ser un número. Intente nuevamente.')
+        if valor_ref <= 0:
+            datos_validos = False
+            print('El valor de referencia debe ser un número positivo. Intente nuevamente.')
+        if vol_act < 0:
+            datos_validos = False
+            print('El volumen de actividad no puede ser negativo. Intente nuevamente.')
+        if met_op < 1 or met_op > 4:
+            datos_validos = False
+            print('La metodología de operación debe ser un número entre 1 y 4. Intente nuevamente.')
+        if unidades < 0:
+            datos_validos = False
+            print('Las unidades totales en tesorería no pueden ser negativas. Intente nuevamente.')
+        if punt_conf < 1 or punt_conf > 10:
+            datos_validos = False
+            print('El puntaje de confianza debe ser un número entre 1 y 10. Intente nuevamente.')
+
+        if datos_validos:
+            tabla_activos.append(activo)
+            print('Datos del activo agregados correctamente.')
+        else:
+            print('Datos inválidos. Intente nuevamente.')
+
         nombre = input('Ingrese el nombre oficial del activo o fin para finalizar: ')
     
     return tabla_activos
