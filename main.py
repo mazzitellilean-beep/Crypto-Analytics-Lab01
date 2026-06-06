61543d8318ec5499c70d377027298eb64cdaa6ea
Federico López Rubio <fedelopezrubio@gmail.com>
2026-06-06 16:31:43 -0300

    Import de generacion tabla
    Se importa la funcion de generacion_tabla directamente en el main

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from mostrar_menu import menu
+from generacion_tabla import generacion_tabla
+    
+tabla_activos = generacion_tabla()
 
 opcion = menu()
 
 while opcion != 8:
     from validacion_opcion import verificacion_menu
-    verificacion_menu(opcion)
+    verificacion_menu(opcion, tabla_activos)
     opcion = int(input('Ingrese el numero de la opcion que desea ejecutar: '))
 
 print("Gracias por utilizar el sistema de gestion. Finalizando programa...")
