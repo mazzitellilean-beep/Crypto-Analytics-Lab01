33bbf657175572833e6fa967ac4fb709c30ac6e8
Federico López Rubio <fedelopezrubio@gmail.com>
2026-06-06 13:45:17 -0300

    Import menu
    Importamos la funcion de menu al main

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ from carga_matriz import carga_matriz
 from modificar_activo import modificar_activo
 from baja_activo import baja_activo
 from mostrar_matriz import mostrar_tabla
+from mostrar_menu import menu
