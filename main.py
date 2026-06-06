b6b1ce9106c3310dfc55c9726faa72926ff6e8eb
Federico López Rubio <fedelopezrubio@gmail.com>
2026-06-06 13:49:43 -0300

    Correciones en los nombres de los imports
    Se corrigieron los nombres de las funciones que se importaban de mostrar_matriz; baja_activo; modificar_activo

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from carga_matriz import carga_matriz
-from modificar_activo import modificar_activo
-from baja_activo import baja_activo
-from mostrar_matriz import mostrar_tabla
+from modificar_activo import modificar
+from baja_activo import eliminar
+from mostrar_matriz import mostrar_matriz
 from mostrar_menu import menu
+
+menu()
