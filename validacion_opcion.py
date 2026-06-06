2b906b57a6aadc2722244f7839313de385a38e9b
Federico López Rubio <fedelopezrubio@gmail.com>
2026-06-06 16:51:20 -0300

    Return lista y nuevos parametros
    Ahora se devuelve la lista, que bien pudo haber sido modificada. Tambien se iguala el parametro lista a cada una de las funciones que la modifica, y a estas mismas funciones se le aniaden los parametros lista

--- validacion_opcion.py
+++ validacion_opcion.py
@@ -2,15 +2,17 @@ def verificacion_menu (opcion, lista):
     
     if opcion == 1:
         from carga_matriz import carga_matriz
-        carga_matriz()
+        lista = carga_matriz()
     elif opcion == 2:
         from baja_activo import eliminar
-        eliminar()
+        lista = eliminar(lista)
     elif opcion == 3:
         from modificar_activo import modificar
-        modificar()
+        lista = modificar(lista)
     elif opcion == 4:
         from mostrar_matriz import mostrar_matriz
         mostrar_matriz(lista)
     else:
         print("Opcion no valida. Por favor, ingrese una opcion del menu.")
+    
+    return lista
