2323689836135223c7a674941111aa6e307be037
Federico López Rubio <fedelopezrubio@gmail.com>
2026-06-06 19:11:47 -0300

    Formato de muestra mas prolijo

--- mostrar_matriz.py
+++ mostrar_matriz.py
@@ -3,4 +3,4 @@ def mostrar_matriz (lista):
     print('Nombre del activo | Ticker | Valor de referencia | Volumen de actividad | Metodología de operación | Unidades totales en tesorería | Puntaje de confianza')
 
     for i in lista:
-        print(lista[i])
+        print(i[0], '|', i[1], '|', i[2], '|', i[3], '|', i[4], '|', i[5], '|', i[6])
