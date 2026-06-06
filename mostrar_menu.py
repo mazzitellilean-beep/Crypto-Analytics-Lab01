225048ca7124ebd7feba5e9b48879be7e4cd9d0c
Federico López Rubio <fedelopezrubio@gmail.com>
2026-06-06 13:35:10 -0300

    Menu
    Mostramos el menu por pantalla. Se muestran las opciones de: Registrar nuevo activo (Alta); Eliminar activo del sistema(Baja); Modificar valoracion o puntaje (Modificacion); Informacion General - Visualizacion de los Datos; y Salir.

---
+++ mostrar_menu.py
@@ -0,0 +1,9 @@
+def menu():
+    print('='*50)
+    print(' '*2 + 'SISTEMA DE GESTION: CRYPTO-ANALYTICS LAB')
+    print('='*50)
+    print("1. Cargar matriz")
+    print("2. Modificar activo")
+    print("3. Dar de baja activo")
+    print("4. Mostrar tabla")
+    print("8. Salir")
