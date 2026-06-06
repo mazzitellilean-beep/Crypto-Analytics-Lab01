725f9aefa12d0c6a48193ebd5911e001ff45c57b
Federico López Rubio <fedelopezrubio@gmail.com>
2026-06-06 15:47:57 -0300

    Analisis de la opcion
    Segun la opcion ingresada se ejecutan las distintas funciones.

---
+++ validacion_opcion.py
@@ -0,0 +1,15 @@
+def verificacion_menu (opcion):
+    if opcion == 1:
+        from carga_matriz import carga_matriz
+        carga_matriz()
+    elif opcion == 2:
+        from baja_activo import eliminar
+        eliminar()
+    elif opcion == 3:
+        from modificar_activo import modificar
+        modificar()
+    elif opcion == 4:
+        from mostrar_matriz import mostrar_matriz
+        mostrar_matriz()
+    else:
+        print("Opcion no valida. Por favor, ingrese una opcion del menu.")
