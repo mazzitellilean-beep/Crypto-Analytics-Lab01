22e2f39c46da70617212db5441251f52e372ddc7
Federico López Rubio <fedelopezrubio@gmail.com>
2026-06-06 18:16:33 -0300

    Fin de ciclo
    Al finalizar la iteracion se vuelve a pedir el ticker, de otra forma el ciclo no finalizaba

--- baja_activo.py
+++ baja_activo.py
@@ -7,5 +7,7 @@ def eliminar(lista, ticker):
                 lista.remove(lista[i])
             elif lista[i][1].upper() != ticker.upper():
                 print('El ticker ingresado no coincide con ningún activo en la tabla. Intente nuevamente')
+        
+        ticker = input('Ingrese el ticker del activo que desea eliminar o fin para finalizar: ')
 
     return lista
