c2fab8e758c1d8932fa64cb736c3c28a2f806e7e
Federico López Rubio <fedelopezrubio@gmail.com>
2026-06-06 19:37:29 -0300

    Correcta forma de recorrer la matriz
    Otro error de indices. Ahora tomamos i como fila, y cada elemento entre corchetes. No trabajamos con tuplas

--- baja_activo.py
+++ baja_activo.py
@@ -3,9 +3,9 @@ def eliminar(lista, ticker):
     while ticker != 'fin':
         for i in lista: 
 
-            if lista[i][1].upper() == ticker.upper() and lista[i][5] == 0:
-                lista.remove(lista[i])
-            elif lista[i][1].upper() != ticker.upper():
+            if i[1].upper() == ticker.upper() and i[5] == 0:
+                lista.remove(i)
+            elif i[1].upper() != ticker.upper():
                 print('El ticker ingresado no coincide con ningún activo en la tabla. Intente nuevamente')
         
         ticker = input('Ingrese el ticker del activo que desea eliminar o fin para finalizar: ')
