2f9d6721e323611a597074fdcaeceb27213a33fa
Federico López Rubio <fedelopezrubio@gmail.com>
2026-06-06 18:09:00 -0300

    Ticker no coincide
    Condicion en caso de el ticker no exista en la tabla

--- baja_activo.py
+++ baja_activo.py
@@ -1,10 +1,10 @@
 def eliminar(lista, ticker):
 
-    for i in lista:
+    for i in lista: 
 
         if lista[i][1].upper() == ticker.upper() and lista[i][5] == 0:
-
-
             lista.remove(lista[i])
+        elif lista[i][1].upper() != ticker.upper():
+            print('El ticker ingresado no coincide con ningún activo en la tabla. Intente nuevamente')
 
     return lista
