5e130403d89337b42684dee79a829f6e22bf8eaf
Federico López Rubio <fedelopezrubio@gmail.com>
2026-06-06 19:45:49 -0300

    Aplicacion de bandera en caso de ticker no encontrado
    Para evitar que se imprima tantas veces el mensaje de error, si no se encuentra el ticker o hay unidades en tesoreria, se guarda True en una bandera.
    Recien fuera del ciclo for, si esa bandera es True se muestra el mensaje de error, de otra forma se informa que el activo fue correctamente eliminado

--- baja_activo.py
+++ baja_activo.py
@@ -1,12 +1,19 @@
 def eliminar(lista, ticker):
 
     while ticker != 'fin':
+        ticker_no_encontrado = False
         for i in lista: 
 
             if i[1].upper() == ticker.upper() and i[5] == 0:
                 lista.remove(i)
+                ticker_no_encontrado = False
             elif i[1].upper() != ticker.upper():
-                print('El ticker ingresado no coincide con ningún activo en la tabla. Intente nuevamente')
+                ticker_no_encontrado = True
+
+        if ticker_no_encontrado:
+            print('Ticker no encontrado o el activo tiene unidades en tesorería. Intente nuevamente.')
+        else:
+            print('El activo fue correctamente eliminado')
         
         ticker = input('Ingrese el ticker del activo que desea eliminar o fin para finalizar: ')
 
