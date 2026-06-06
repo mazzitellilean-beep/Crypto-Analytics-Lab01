6b74e6f141a6ec95cd40703064167c4ba186684b
Federico López Rubio <fedelopezrubio@gmail.com>
2026-06-06 19:35:26 -0300

    Ticker deja de ser int
    Antes al pedir que se ingrese el ticker, se convertia en entero, lo que tiraba error

--- validacion_opcion.py
+++ validacion_opcion.py
@@ -5,7 +5,7 @@ def verificacion_menu (opcion, lista):
         lista = carga_matriz()
     elif opcion == 2:
         from baja_activo import eliminar
-        ticker = int(input('Ingrese el ticker del activo que desea eliminar: '))
+        ticker = input('Ingrese el ticker del activo que desea eliminar: ')
         lista = eliminar(lista, ticker)
     elif opcion == 3: 
         from modificar_activo import modificar
