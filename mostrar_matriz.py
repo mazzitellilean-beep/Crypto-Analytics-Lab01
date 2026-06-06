906cc5e09df5262c50087397d4dbdab7d6ef95ed
Federico López Rubio <fedelopezrubio@gmail.com>
2026-06-06 18:35:37 -0300

    i como lista
    Ahora si, tomamos i como cada lista y la imprimimos. De otra forma teniamos un index error

--- mostrar_matriz.py
+++ mostrar_matriz.py
@@ -3,4 +3,4 @@ def mostrar_matriz (lista):
     print('Nombre del activo | Ticker | Valor de referencia | Volumen de actividad | Metodología de operación | Unidades totales en tesorería | Puntaje de confianza')
 
     for i in lista:
-        print(f'{lista[i]}')
+        print(lista[i])
