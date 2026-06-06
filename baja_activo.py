83c5f795cdd252780dc10f87a07b3e71d17524bc
Federico López Rubio <fedelopezrubio@gmail.com>
2026-06-06 18:14:58 -0300

    Pedir varios tickers
    Una vez que se elimina un ticker, si no se escribe fin, pueden seguir ingresandose tickers para eliminar

--- baja_activo.py
+++ baja_activo.py
@@ -1,10 +1,11 @@
 def eliminar(lista, ticker):
 
-    for i in lista: 
+    while ticker != 'fin':
+        for i in lista: 
 
-        if lista[i][1].upper() == ticker.upper() and lista[i][5] == 0:
-            lista.remove(lista[i])
-        elif lista[i][1].upper() != ticker.upper():
-            print('El ticker ingresado no coincide con ningún activo en la tabla. Intente nuevamente')
+            if lista[i][1].upper() == ticker.upper() and lista[i][5] == 0:
+                lista.remove(lista[i])
+            elif lista[i][1].upper() != ticker.upper():
+                print('El ticker ingresado no coincide con ningún activo en la tabla. Intente nuevamente')
 
     return lista
