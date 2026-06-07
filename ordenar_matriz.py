79d3eea9951bf70e8c0380f682ce5758cbeeb7fa
Federico López Rubio <fedelopezrubio@gmail.com>
2026-06-07 13:34:07 -0300

    Orden alfabetico
    Al orden por puntaje le sumamos el criterio de ordenar alfabeticamente en caso de que dos activos tengan el mismo puntaje

--- ordenar_matriz.py
+++ ordenar_matriz.py
@@ -6,6 +6,11 @@ def ordenar_matriz(lista):
         for j in range(i+1, len(lista)):
             if lista[j][6] > lista[indice_max][6]:
                 indice_max = j
+            elif lista[j][6] == lista[indice_max][6]:
+                nombre_activo = lista[j][0].upper()
+                nombre_activo_max = lista[indice_max][0].upper()
+                if nombre_activo < nombre_activo_max:
+                    indice_max = j
             
         lista[i], lista[indice_max] = lista[indice_max], lista[i]
 
