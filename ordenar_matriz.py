e1f84614ef19811671ad51e63bafad4687cd2a25
Federico López Rubio <fedelopezrubio@gmail.com>
2026-06-07 12:36:30 -0300

    Metodo de seleccion
    Para ordenar la matriz por puntaje de mayor a menor utilizamos el metodo de seleccion

---
+++ ordenar_matriz.py
@@ -0,0 +1,10 @@
+def ordenar_matriz(lista):
+
+    for i in range(len(lista)-1):
+        indice_max = i
+
+        for j in range(i+1, len(lista)):
+            if lista[j][6] > lista[indice_max][6]:
+                indice_max = j
+            
+        lista[i], lista[indice_max] = lista[indice_max], lista[i]
