47998af57da47f015f403cf164bab6cdd7bccc55
Federico López Rubio <fedelopezrubio@gmail.com>
2026-06-07 21:23:14 -0300

    mensaje de activo eliminado
    Agregando el break despues de encontrar el ticker permite imprimir el mensaje de activo correctamente eliminado y no seguir buscando para que no se modifique la bandera e imprima mal

--- baja_activo.py
+++ baja_activo.py
@@ -7,13 +7,13 @@ def eliminar(lista, ticker):
             if i[1].upper() == ticker.upper() and i[5] == 0:
                 lista.remove(i)
                 ticker_no_encontrado = False
-            elif i[1].upper() != ticker.upper():
+                print('El activo fue correctamente eliminado')  
+                break
+            elif i[1].upper() != ticker.upper() or i[5] > 0:
                 ticker_no_encontrado = True
 
         if ticker_no_encontrado:
-            print('Ticker no encontrado o el activo tiene unidades en tesorería. Intente nuevamente.')
-        else:
-            print('El activo fue correctamente eliminado')
+            print('Ticker no encontrado o el activo tiene unidades en tesorería. Intente nuevamente.')      
         
         ticker = input('Ingrese el ticker del activo que desea eliminar o fin para finalizar: ')
 
